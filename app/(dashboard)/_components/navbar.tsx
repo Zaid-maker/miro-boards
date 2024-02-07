cc8d6e806e93fb2b6e8601853d1bcc2608a58cf1
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-02-07 14:11:39 +0000

    update navbar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,5 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex items-center gap-x-4 p-5">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
