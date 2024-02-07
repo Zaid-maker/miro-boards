767f7bd874738927f38b6d368fd4a95a41d3d2e5
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-02-07 13:58:27 +0000

    create OrgSidebar component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Sidebar } from "./_components/sidebar";
+import { OrgSidebar } from "./_components/org-sidebar";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
@@ -9,7 +10,15 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <main className="h-full">
       <Sidebar />
-      <div className="pl-[60px] h-full">{children}</div>
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">
+            {/*<Navbar />*/}
+            {children}
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
