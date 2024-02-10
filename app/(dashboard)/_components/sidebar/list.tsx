36e44e721bb96ba8cebca177c7914ea9b33b5af0
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-02-10 11:18:29 +0000

    fix

--- app/(dashboard)/_components/sidebar/list.tsx
+++ app/(dashboard)/_components/sidebar/list.tsx
@@ -12,10 +12,10 @@ export const List = () => {
   if (!userMemberships.data?.length) return null;
 
   return (
-    <ul>
-      {userMemberships.data?.map((mem) => {
-        <p key={mem.organization.id}>{mem.organization.name}</p>;
-      })}
+    <ul className="space-y-4">
+      {userMemberships.data?.map((mem) => (
+        <p key={mem.organization.id}>{mem.organization.name}</p>
+      ))}
     </ul>
   );
 };
