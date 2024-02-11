732905eaebcd3e94c63117bbef819d24e32aea0f
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-02-11 18:31:26 +0000

    update

--- app/(dashboard)/_components/sidebar/item.tsx
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import Image from "next/image";
 import React from "react";
 
 interface ItemProps {
@@ -9,5 +12,22 @@ interface ItemProps {
 }
 
 export const Item = ({ id, name, imageUrl }: ItemProps) => {
-  return <div>Item</div>;
+  const { organization } = useOrganization();
+  const { setActive } = useOrganizationList();
+
+  const isActive = organization?.id === id;
+
+  return (
+    <div className="aspect-square relative">
+      <Image
+        src={imageUrl}
+        alt={name}
+        fill
+        className={cn(
+          "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
+          isActive && "opacity-100"
+        )}
+      />
+    </div>
+  );
 };
