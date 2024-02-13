14827cb0a171118e4424de67845dfcd4a04d2d27
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-02-13 23:44:36 +0500

    create SearchInput component for navbar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,10 +1,11 @@
-import { UserButton } from "@clerk/nextjs";
-import React from "react";
+import { SearchInput } from "./search-input";
 
 export const Navbar = () => {
   return (
     <div className="flex items-center gap-x-4 p-5">
-      <UserButton afterSignOutUrl="/" />
+      <div className="hidden lg:flex lg:flex-1">
+        <SearchInput />
+      </div>
     </div>
   );
 };
---
+++ app/(dashboard)/_components/search-input.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const SearchInput = () => {
+  return <div>SearchInput</div>;
+};
