3e23ef5637155eb60ea165e7edb2c04bffda5d99
DevMirza <pzhafeez@gmail.com>
2024-02-13 10:24:47 +0000

    complete

--- app/(dashboard)/_components/sidebar/item.tsx
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Hint } from "@/components/hont";
 import { cn } from "@/lib/utils";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 import Image from "next/image";
@@ -17,17 +18,26 @@ export const Item = ({ id, name, imageUrl }: ItemProps) => {
 
   const isActive = organization?.id === id;
 
+  const onClick = () => {
+    if (!setActive) return;
+
+    setActive({ organization: id });
+  };
+
   return (
     <div className="aspect-square relative">
-      <Image
-        src={imageUrl}
-        alt={name}
-        fill
-        className={cn(
-          "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
-          isActive && "opacity-100"
-        )}
-      />
+      <Hint label={name} side="right" align="start" sideOffset={18}>
+        <Image
+          src={imageUrl}
+          alt={name}
+          onClick={onClick}
+          fill
+          className={cn(
+            "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition",
+            isActive && "opacity-100"
+          )}
+        />
+      </Hint>
     </div>
   );
 };
