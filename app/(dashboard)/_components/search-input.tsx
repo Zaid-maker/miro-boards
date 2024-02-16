4ab042429750cb286a91f8057a4e45f5a7ba54f4
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-02-16 23:14:51 +0500

    fix style

--- app/(dashboard)/_components/search-input.tsx
+++ app/(dashboard)/_components/search-input.tsx
@@ -34,7 +34,7 @@ export const SearchInput = () => {
     <div className="w-full relative">
       <Search className="absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground h-4 w-4" />
       <Input
-        className="w-full max-w-[516px] pl-6"
+        className="w-full max-w-[516px] pl-9"
         placeholder="Search Boards..."
         onChange={handleChange}
         value={value}
