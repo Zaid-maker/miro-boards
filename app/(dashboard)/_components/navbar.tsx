a38e5dc89942959f4cd625cb293ca49c63124d0f
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-02-16 19:23:42 +0500

    update Navbar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,3 +1,4 @@
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { SearchInput } from "./search-input";
 
 export const Navbar = () => {
@@ -6,6 +7,30 @@ export const Navbar = () => {
       <div className="hidden lg:flex lg:flex-1">
         <SearchInput />
       </div>
+      <div className="block lg:hidden flex-1">
+        <OrganizationSwitcher
+          hidePersonal
+          appearance={{
+            elements: {
+              rootBox: {
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                maxWidth: "376px",
+              },
+              organizationSwitcherTrigger: {
+                padding: "6px",
+                width: "100%",
+                borderRadius: "8px",
+                border: "1px solid #E5E7EB",
+                backgroundColor: "white",
+              },
+            },
+          }}
+        />
+      </div>
+      <UserButton />
     </div>
   );
 };
