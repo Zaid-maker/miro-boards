d1db0c0536e6711a443f0d58385dcb3ec8d0b1bb
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-02-18 16:02:44 +0500

    wrap Toaster in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "@/providers/convex-client-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ConvexClientProvider } from "@/providers/convex-client-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ConvexClientProvider>
+          <Toaster />
+          {children}
+        </ConvexClientProvider>
       </body>
     </html>
   );
