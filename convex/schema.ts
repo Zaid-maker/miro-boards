6830f4ba57c3a25d17f6a9cc6b470e2dc725053c
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-02-18 17:47:38 +0500

    format

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,5 +1,5 @@
-import { defineSchema, defineTable } from 'convex/server'
-import { v } from 'convex/values'
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
 
 export default defineSchema({
     boards: defineTable({
@@ -7,18 +7,20 @@ export default defineSchema({
         orgId: v.string(),
         authorId: v.string(),
         authorName: v.string(),
-        imageUrl: v.string()
-    }).index("by_org", ["orgId"]).searchIndex("search_title", {
-        searchField: "title",
-        filterFields: ["orgId"]
-    }),
+        imageUrl: v.string(),
+    })
+        .index("by_org", ["orgId"])
+        .searchIndex("search_title", {
+            searchField: "title",
+            filterFields: ["orgId"],
+        }),
     userFavorites: defineTable({
         orgId: v.string(),
         userId: v.string(),
-        boardId: v.id("boards")
+        boardId: v.id("boards"),
     })
         .index("by_board", ["boardId"])
         .index("by_user_org", ["userId", "orgId"])
         .index("by_user_board", ["userId", "boardId"])
-        .index("by_user_board_org", ["userId", "boardId", "orgId"])
-})
+        .index("by_user_board_org", ["userId", "boardId", "orgId"]),
+});
