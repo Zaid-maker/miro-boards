133156daa50134bbc1d76b714ee04eb93d6b150d
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-02-18 16:34:05 +0500

    create BoardCard

---
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface BoardCardProps {
+  id: string;
+  title: string;
+  authorName: string;
+  authorId: string;
+  createdAt: number;
+  imageUrl: string;
+  orgId: string;
+  isFavorite: boolean;
+}
+
+export const BoardCard = ({}: BoardCardProps) => {
+  return <div>BoardCard</div>;
+};
--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -6,6 +6,7 @@ import { EmptyFavorites } from "./empty-favorites";
 import { EmptySearch } from "./empty-search";
 import { NewBoardButton } from "./new-board-button";
 import { api } from "@/convex/_generated/api";
+import { BoardCard } from "./board-card";
 
 interface BoardListProps {
   orgId: string;
@@ -50,6 +51,19 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
         <NewBoardButton orgId={orgId} />
+        {data?.map((board) => (
+          <BoardCard
+            key={board._id}
+            id={board._id}
+            title={board.title}
+            imageUrl={board.imageUrl}
+            authorId={board.authorId}
+            authorName={board.authorName}
+            createdAt={board._creationTime}
+            orgId={board.orgId}
+            isFavorite={board.isFavorite}
+          />
+        ))}
       </div>
     </div>
   );
