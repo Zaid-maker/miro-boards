d8b0a0f5800329127e7d28f05448267c549f1b24
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-02-21 23:46:24 +0500

    create Actions component

---
+++ components/actions.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+import React from "react";
+
+interface ActionsProps {
+  children: React.ReactNode;
+  side?: DropdownMenuContentProps["side"];
+  sideOffset?: DropdownMenuContentProps["sideOffset"];
+  id: string;
+  title: string;
+}
+
+export const Actions = ({
+  children,
+  side,
+  sideOffset,
+  id,
+  title,
+}: ActionsProps) => {
+  return <div>Actions</div>;
+};
