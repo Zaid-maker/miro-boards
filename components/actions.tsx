a8daba3d4e71ff04d66a7d3b4868d05da9b0e870
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-02-22 23:18:54 +0500

    update Action compoent

--- components/actions.tsx
+++ components/actions.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+import { Link2, Pencil } from "lucide-react";
 import React from "react";
+import { toast } from "sonner";
 
 interface ActionsProps {
   children: React.ReactNode;
@@ -18,5 +26,31 @@ export const Actions = ({
   id,
   title,
 }: ActionsProps) => {
-  return <div>Actions</div>;
+  const onCopyLink = () => {
+    navigator.clipboard
+      .writeText(`${window.location.origin}/board/${id}`)
+      .then(() => toast.success("Link Copied"))
+      .catch(() => toast.error("Failed to copy board link"));
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent
+        onClick={(e) => e.stopPropagation()}
+        side={side}
+        sideOffset={sideOffset}
+        className="w-60"
+      >
+        <DropdownMenuItem onClick={onCopyLink} className="p-3 cursor-pointer">
+          <Link2 className="h-4 w-4 mr-2" />
+          Copy board link
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => {}} className="p-3 cursor-pointer">
+          <Pencil className="h-4 w-4 mr-2" />
+          Rename
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
