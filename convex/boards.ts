f1b5d776bf295a54c14333c8a351d6d0cad3dea8
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-03-06 21:21:28 +0000

    update...i am dizzy rn

--- convex/boards.ts
+++ convex/boards.ts
@@ -1,6 +1,7 @@
 import { v } from "convex/values";
-import { query } from "./_generated/server";
 import { getAllOrThrow } from "convex-helpers/server/relationships";
+
+import { query } from "./_generated/server";
 
 export const get = query({
     args: {
@@ -9,9 +10,9 @@ export const get = query({
         favorites: v.optional(v.string()),
     },
     handler: async (ctx, args) => {
-        const identify = await ctx.auth.getUserIdentity();
+        const identity = await ctx.auth.getUserIdentity();
 
-        if (!identify) {
+        if (!identity) {
             throw new Error("Unauthorized");
         }
 
@@ -19,17 +20,56 @@ export const get = query({
             const favoritedBoards = await ctx.db
                 .query("userFavorites")
                 .withIndex("by_user_org", (q) =>
-                    q.eq("userId", identify.subject).eq("orgId", args.orgId)
-                ).order('desc').collect();
+                    q.eq("userId", identity.subject).eq("orgId", args.orgId)
+                )
+                .order("desc")
+                .collect();
 
-            const ids = favoritedBoards.map((b) => b.boardId)
+            const ids = favoritedBoards.map((b) => b.boardId);
 
-            const boards = await getAllOrThrow(ctx.db, ids)
+            const boards = await getAllOrThrow(ctx.db, ids);
 
             return boards.map((board) => ({
                 ...board,
-                isFavorite: true
-            }))
+                isFavorite: true,
+            }));
         }
+
+        const title = args.search as string;
+        let boards = [];
+
+        if (title) {
+            boards = await ctx.db
+                .query("boards")
+                .withSearchIndex("search_title", (q) =>
+                    q.search("title", title).eq("orgId", args.orgId)
+                )
+                .collect();
+        } else {
+            boards = await ctx.db
+                .query("boards")
+                .withIndex("by_org", (q) => q.eq("orgId", args.orgId))
+                .order("desc")
+                .collect();
+        }
+
+        const boardsWithFavoriteRelation = boards.map((board) => {
+            return ctx.db
+                .query("userFavorites")
+                .withIndex("by_user_board", (q) =>
+                    q.eq("userId", identity.subject).eq("boardId", board._id)
+                )
+                .unique()
+                .then((favorite) => {
+                    return {
+                        ...board,
+                        isFavorite: !!favorite,
+                    };
+                });
+        });
+
+        const boardsWithFavoriteBoolean = Promise.all(boardsWithFavoriteRelation);
+
+        return boardsWithFavoriteBoolean;
     },
 });
