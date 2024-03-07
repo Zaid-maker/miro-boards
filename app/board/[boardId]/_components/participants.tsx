4c5136e122c326d48f348ba00c8b9ef2f2ae2ca3
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-03-07 18:49:50 +0000

    update

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Participants = () => {
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-      Participants
+      <div className="flex gap-x-2">Participants in Room</div>
     </div>
   );
 };
