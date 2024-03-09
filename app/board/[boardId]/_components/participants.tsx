c840632f62436441d89ff6f0164bb17c3600b97f
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-03-09 18:33:55 +0000

    Update Participants

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,9 +1,22 @@
+"use client";
+
+import { useOthers, useSelf } from "@/liveblocks.config";
 import React from "react";
 
+const MAX_SHOWN_USERS = 2;
+
 export const Participants = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_USERS;
+
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-      <div className="flex gap-x-2">Participants in Room</div>
+      <div className="flex gap-x-2">
+        {users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info }) => {
+          return <p key={connectionId}>{info?.picture}</p>;
+        })}
+      </div>
     </div>
   );
 };
